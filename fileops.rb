8f4bf6cc1b8b5e14f5a10b4aacc4678047f0a3d1
Kevin F Browne <kbrowneatx@gmail.com>
2013-05-12 11:30:37 -0500

    changed fileops

--- fileops.rb
+++ fileops.rb
@@ -2,7 +2,7 @@ module fileops
 	def getpeople
 	  # get a listing of all the people
 	  people = []
-	  files = Dir.entries('c:\sites\sinatra_json\people')
+	  files = Dir.entries('/people')
 	  files.each do |f|	
 		if f != "." && f != ".."
 		  people.push(f.slice(0..f.index(".")-1))
@@ -15,7 +15,7 @@ module fileops
 	def getperson(name)
 	  # just get one person
 	  # in production use filecontents = IO.read('people/#{name}.json')
-	  filecontents = IO.read('c:\sites\sinatra_json\people\kevin.json')
+	  filecontents = IO.read('/people/kevin.json')
 	  person = JSON.parse(filecontents)
 	  puts person
 	  #output the list of favs in json
